001abe5e9787ca38807c714441c4c78747dcfb66
feiyu <feiyu@tg10010.com>
2019-05-08 20:25:12 +0700

    sas

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,30 +1,87 @@
-import React, { Component } from 'react'
-import {Skeleton} from 'antd'
+import React, { Component } from 'react';
+import { Skeleton } from 'antd';
 import { Rate } from 'antd';
 
 const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
 
-export default class index extends Component {
-  state={
-    value:3
+function Welcome(props) {
+  // console.log(props);
+  return <h1>hello,{props.name}</h1>;
+}
+
+function ActionLink(props) {
+  function handleClick(e) {
+    e.preventDefault();
+    const data = [1, 2, 3];
+    props.handleClickqq(data);
   }
 
-  handleChange = (value)=>{
-    this.setState({
-      value
-    })
+  return (
+    <a href="http://www.jd.com" onClick={handleClick}>
+      Click me
+    </a>
+  );
+}
+
+export default class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 3,
+      date: new Date().toLocaleTimeString(),
+    };
+    // 为了在回调中使用 `this`，这个绑定是必不可少的,或者用箭头函数就不用写这个绑定this
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(value) {
+    this.setState({
+      value,
+    });
+  }
+
+  tick() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date}.</h2>
+      </div>
+    );
+  }
+
+  // 接受子组件的data值
+  handleClickle(data) {
+    console.log(data, '---');
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({
+        date: new Date().toLocaleTimeString(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
-    const {value} = this.state
+    const { value } = this.state;
 
     return (
       <div>
-       <Skeleton active></Skeleton>
-       <span>
-        <Rate tooltips={desc} onChange={this.handleChange} value={value} />
-        {value ? <span className="ant-rate-text">{desc[value - 1]}</span> : ''}
-      </span>
+        <Skeleton active />
+        <Welcome name="reactApp" />
+        <Welcome name="feiyu" />
+        <span>
+          <Rate tooltips={desc} onChange={this.handleChange} value={value} />
+          {value ? <span className="ant-rate-text">{desc[value - 1]}</span> : ''}
+        </span>
+        {this.tick()}
+        {/* handleClickqq就是props传给子组件的key值 */}
+        <ActionLink handleClickqq={this.handleClickle} />
       </div>
-    )
+    );
   }
 }
--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,72 +1,101 @@
-import React, { Component } from 'react'
-import {Table, Divider, Tag } from 'antd'
+import React, { Component } from 'react';
+import { Table, Divider, Tag } from 'antd';
+import router from 'umi/router';
 
-const columns = [{
-  title: 'Name',
-  dataIndex: 'name',
-  key: 'name',
-  render: text => <a href="javascript:;">{text}</a>,
-}, {
-  title: 'Age',
-  dataIndex: 'age',
-  key: 'age',
-}, {
-  title: 'Address',
-  dataIndex: 'address',
-  key: 'address',
-}, {
-  title: 'Tags',
-  key: 'tags',
-  dataIndex: 'tags',
-  render: tags => (
-    <span>
-      {tags.map(tag => {
-        let color = tag.length > 5 ? 'geekblue' : 'green';
-        if (tag === 'loser') {
-          color = 'volcano';
-        }
-        return <Tag color={color} key={tag}>{tag.toUpperCase()}</Tag>;
-      })}
-    </span>
-  ),
-}, {
-  title: 'Action',
-  key: 'action',
-  render: (text, record) => (
-    <span>
-      <a href="javascript:;">Invite {record.name}</a>
-      <Divider type="vertical" />
-      <a href="javascript:;">Delete</a>
-    </span>
-  ),
-}];
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <i>{text}</i>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <span>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </span>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <i>Invite {record.name}</i>
+        <Divider type="vertical" />
+        <i>Delete</i>
+      </span>
+    ),
+  },
+];
 
-const data = [{
-  key: '1',
-  name: 'John Brown',
-  age: 32,
-  address: 'New York No. 1 Lake Park',
-  tags: ['nice', 'developer'],
-}, {
-  key: '2',
-  name: 'Jim Green',
-  age: 42,
-  address: 'London No. 1 Lake Park',
-  tags: ['loser'],
-}, {
-  key: '3',
-  name: 'Joe Black',
-  age: 32,
-  address: 'Sidney No. 1 Lake Park',
-  tags: ['cool', 'teacher'],
-}];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map(number => <li key={number.toString()}>{number}</li>);
+  //console.log(listItems);
+  return <ul>{listItems}</ul>;
+}
 
 export default class index extends Component {
+  jumpUrl() {
+    router.push({ pathname: '/about' });
+  }
+
   render() {
+    const numbers = [1, 3, 5, 8, 9];
     return (
       <div>
         <Table columns={columns} dataSource={data} />
+        <button onClick={this.jumpUrl.bind(this)}>页面跳转</button>
+        <NumberList numbers={numbers} />
       </div>
-    )
+    );
   }
 }
