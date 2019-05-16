81a2c1feaca03af63348d988996a4fd975e11c21
feiyu <feiyu@tg10010.com>
2019-05-16 15:12:24 +0700

    sss

--- src/pages/getDate/index.js
+++ src/pages/getDate/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import axios from '@/libs/axios';
+import { Table } from 'antd';
 
 export default class GetData extends Component {
   constructor(props) {
     super(props);
-    this.state = [];
+    this.state = {
+      dataSource: [],
+    };
   }
 
   getData() {
@@ -17,6 +20,13 @@ export default class GetData extends Component {
           console.log(res.statusText);
           return;
         }
+
+        res.data.forEach(element => {
+          element.key = element.id;
+        });
+        this.setState({
+          dataSource: res.data,
+        });
         console.log(res.data);
       })
       .catch(err => {
@@ -28,6 +38,32 @@ export default class GetData extends Component {
     this.getData();
   }
   render() {
-    return <div />;
+    const columns = [
+      {
+        title: '项目名称',
+        dataIndex: 'name',
+      },
+      {
+        title: '文件大小',
+        dataIndex: 'size',
+        render: (text, record, index) => {
+          return <span>{text}KB</span>;
+        },
+      },
+      {
+        title: '项目描述',
+        dataIndex: 'description',
+      },
+      {
+        title: '地址',
+        dataIndex: 'url',
+      },
+    ];
+
+    return (
+      <div>
+        <Table columns={columns} dataSource={this.state.dataSource} />
+      </div>
+    );
   }
 }
