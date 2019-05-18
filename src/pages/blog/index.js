059f1e2389aee834d3765b3d414f3f003129952a
feiyu <feiyu@tg10010.com>
2019-05-18 15:25:13 +0700

    'blog'

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
-import { Table } from 'antd';
+import ReactMarkdown from 'react-markdown';
+import { Table, Spin } from 'antd';
 import { get } from '@/libs/axios';
-
+import styles from './index.scss';
 export default class Blog extends Component {
-  state = {};
+  state = {
+    data: [],
+    markDownData: '',
+  };
 
   async getBlogList() {
-    const res = await get('repos/feiyuWeb/Blog/notes');
-    console.log(res);
+    const res = await get('repos/chanshiyucx/blog/issues');
+    res.forEach(element => {
+      element.key = element.id;
+    });
+    this.setState({
+      data: res,
+      markDownData: res[0].body,
+    });
+    //console.log(res);
   }
 
   componentDidMount() {
     this.getBlogList();
   }
+
   render() {
+    const columns = [
+      {
+        title: '创建时间',
+        dataIndex: 'created_at',
+        key: 'created_at',
+      },
+      {
+        title: '标题',
+        dataIndex: 'title',
+        key: 'title',
+      },
+      {
+        title: '更新时间',
+        dataIndex: 'updated_at',
+        key: 'updated_at',
+      },
+      {
+        title: '文章链接',
+        dataIndex: 'html_url',
+        key: 'html_url',
+        render: text => {
+          return (
+            <a href={text} target={'_blank'}>
+              {text}
+            </a>
+          );
+        },
+      },
+    ];
+    const { data, markDownData } = this.state;
     return (
-      <div>
-        <Table />
+      <div className={styles.box}>
+        {data.length === 0 ? <Spin size="large" /> : <Table columns={columns} dataSource={data} />}
+
+        <ReactMarkdown source={markDownData} className={styles.markdown} />
       </div>
     );
   }
